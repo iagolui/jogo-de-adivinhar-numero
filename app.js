97ac88df4d15f46048b6542ab846ea9124016ea4
iagolui <iagoluis1000@gmail.com>
2025-04-14 18:37:09 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 let listaDeNumerosSorteados = [];
-let numeroLimite = 10
+let numeroLimite = 100;
 let numeroSecreto = gerarNumeroAleatorio ();
 let tentativas = 1;
 
@@ -9,7 +9,7 @@ function exibirTextoNaTela(tag, texto) {
     if ('speechSynthesis' in window) {
         let utterance = new SpeechSynthesisUtterance(texto);
         utterance.lang = 'pt-BR'; 
-        utterance.rate = 1.2; 
+        utterance.rate = 1.4; 
         window.speechSynthesis.speak(utterance); 
     } else {
         console.log("Web Speech API não suportada neste navegador.");
